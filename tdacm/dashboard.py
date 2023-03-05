123769ab9a6a8dfd566f605ba4b2edf138f828e4
John Hoban <johnrussell789@gmail.com>
2023-03-05 15:56:33 -0500

    Added sample range options; added spinners

--- tdacm/dashboard.py
+++ tdacm/dashboard.py
@@ -1,6 +1,12 @@
+from functools import partial
+
+import dash
 import dash_bootstrap_components as dbc
+import pandas as pd
 import plotly.express as px
-from dash import dcc, html, Dash, Input, Output
+from dash import dcc, html, Input, Output, State
+from dash_extensions.enrich import DashProxy, MultiplexerTransform
+from pandas.tseries.offsets import Minute, Hour, Day
 
 from tdacm.spclient import SensorPushClient
 
@@ -18,8 +24,13 @@ PLOT_STYLE = dict(
     font={"family": "Helvetica", "color": "#CADBE3"},
 )
 
+TIME_BUTTONS = [f"dashboard-btn-{x}" for x in ("30m", "24h", "7d")]
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = DashProxy(
+    __name__,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    transforms=[MultiplexerTransform()],
+)
 app.layout = html.Div(
     [
         dbc.Navbar(
@@ -29,19 +40,21 @@ app.layout = html.Div(
                     dbc.Col(html.I("Loading...", id="dashboard-timestamp"), width=3),
                     dbc.Col(
                         [
+                            # Start the app in 30M mode (30M button disabled)
                             dbc.Button(
                                 "30M",
-                                id="dashboard-button-30m",
+                                id="dashboard-btn-30m",
                                 class_name="navbar-button",
+                                disabled=True,
                             ),
                             dbc.Button(
                                 "24H",
-                                id="dashboard-button-24h",
+                                id="dashboard-btn-24h",
                                 class_name="navbar-button",
                             ),
                             dbc.Button(
                                 "7D",
-                                id="dashboard-button-7d",
+                                id="dashboard-btn-7d",
                                 class_name="navbar-button",
                             ),
                         ]
@@ -51,24 +64,74 @@ app.layout = html.Div(
             dark=True,
         ),
         dcc.Interval(id="dashboard-interval", interval=60 * 1000),
-        dbc.Row(dcc.Graph(id="dashboard-fig-temperature")),
-        dbc.Row(dcc.Graph(id="dashboard-fig-humidity")),
-        dbc.Row(dcc.Graph(id="dashboard-fig-pressure")),
+        dbc.Row(
+            dbc.Spinner(
+                dcc.Graph(id="dashboard-fig-temperature"), color="info", type="grow"
+            )
+        ),
+        dbc.Row(
+            dbc.Spinner(
+                dcc.Graph(id="dashboard-fig-humidity"), color="info", type="grow"
+            )
+        ),
+        dbc.Row(
+            dbc.Spinner(
+                dcc.Graph(id="dashboard-fig-pressure"), color="info", type="grow"
+            )
+        ),
     ]
 )
+
+
+def update_button_state(n_clicks):
+    """
+    Disable a time-range button when clicked and enable all others
+    """
+    if not n_clicks:
+        return dash.no_update
+    trigger_id = dash.callback_context.triggered_id
+    return [btn_id == trigger_id for btn_id in TIME_BUTTONS]
+
+
+for btn in TIME_BUTTONS:
+    app.callback(
+        [Output(btn_id, "disabled") for btn_id in TIME_BUTTONS],
+        Input(btn, "n_clicks"),
+    )(partial(update_button_state))
 
 
 @app.callback(
     Output("dashboard-fig-temperature", "figure"),
     Output("dashboard-fig-humidity", "figure"),
     Output("dashboard-fig-pressure", "figure"),
+    Output("dashboard-timestamp", "children"),
     Input("dashboard-interval", "n_intervals"),
+    Input("dashboard-btn-30m", "disabled"),
+    State("dashboard-btn-24h", "disabled"),
+    State("dashboard-btn-7d", "disabled"),
 )
-def update_samples(n_intervals):
+def update_samples(n_intervals, active_30m, active_24h, active_7d):
     """
     Automatically the sample graphs at a regular interval
+
+    Sample time series range depends on which time button is clicked/activated
+
+    Can be triggered by the interval component, or by the 30M button changing
+    state, which occurs when any of the three time buttons are clicked
     """
-    df_samples = _SP_CLIENT.get_samples("2023-02-27")
+    now = pd.Timestamp.now()
+    if active_30m:
+        start_dt = now - Minute(30)
+    elif active_24h:
+        start_dt = now - Hour(24)
+    elif active_7d:
+        start_dt = now - Day(7)
+    # App start trigger - 30M
+    else:
+        start_dt = now - Minute(30)
+
+    # Get samples and draw figures
+    df_samples = _SP_CLIENT.get_samples(start_dt)
 
     fig_temperature = px.line(
         df_samples,
@@ -100,4 +163,8 @@ def update_samples(n_intervals):
     fig_pressure.update_layout(PLOT_STYLE)
     fig_pressure.update_yaxes(ticksuffix="mb")
 
-    return fig_temperature, fig_humidity, fig_pressure
+    # Get last update timestamp
+    ts = df_samples.tail(1).time.squeeze()
+    label = f"Last Updated: {ts:%b %m, %Y %#I:%M:%S %p}"
+
+    return fig_temperature, fig_humidity, fig_pressure, label
